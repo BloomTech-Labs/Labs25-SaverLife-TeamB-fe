cd1d3d23e7d91cad2c703cb820f224976482da58
Tony <tonyrkovar@gmail.com>
2020-08-11 17:26:11 -0500

    chore: Testing direct pathing without an index file.

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
-import { SavingsGraph } from '../../graphs/indexGraph';
+import SavingsGraph from '../../graphs/SavingsGraph';
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
