ed75e58ae5d81a5252507c4f8d494132b8af7025
Julia Vu <juliadvu@gmail.com>
2020-08-12 10:30:15 -0500

    Feat:created Budgets graph component.

---
+++ src/components/graphs/Budgets.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+function Budgets() {
+  return (
+    <Plot
+      data={[
+        {
+          mode: 'markers',
+          x: [1, 2, 3, 4],
+          y: [10, 11, 12, 13],
+          text: [
+            'Savings Progress<br>size: 40',
+            'Savings Improved<br>size: 60',
+            'Savings Really Improved<br>size: 80',
+            'Met Savings Goal<br>size: 100',
+          ],
+          marker: {
+            color: [
+              'rgb(93, 164, 214)',
+              'rgb(255, 144, 14)',
+              'rgb(44, 160, 101)',
+              'rgb(255, 65, 54)',
+            ],
+            size: [40, 60, 80, 100],
+          },
+        },
+      ]}
+      layout={{
+        title: 'Bubble Chart Hover Text',
+        showlegend: false,
+        height: 600,
+        width: 600,
+      }}
+    />
+  );
+}
+
+export default Budgets;
