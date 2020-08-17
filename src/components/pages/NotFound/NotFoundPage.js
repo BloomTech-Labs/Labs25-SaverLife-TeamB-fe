dba342dd13bf6451a26f4b34bc05f23713bf9753
kwmorlock <kmorlockcodes@gmail.com>
2020-08-17 17:24:22 -0400

    chore: 404 h2 broken link text added

--- src/components/pages/NotFound/NotFoundPage.js
+++ src/components/pages/NotFound/NotFoundPage.js
@@ -5,6 +5,11 @@ const NotFoundPage = () => {
   return (
     <div>
       <h1>404 Page Not Found</h1>
+      <h2>Sorry for the inconvenience</h2>
+      <h2>
+        The page you followed might have been removed, or the link might be
+        broken
+      </h2>
       <Link to="/">
         <button>Back To Account</button>
       </Link>
