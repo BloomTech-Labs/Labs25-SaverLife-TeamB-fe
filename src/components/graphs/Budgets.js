431490dbb389a5ede5e65434e5f0e5b58f96950c
Henry Nelson <Henry.Nelson89@gmail.com>
2020-08-24 12:33:13 -0700

    Chore:Styled Budget graph

--- src/components/graphs/Budgets.js
+++ src/components/graphs/Budgets.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import styled from 'styled-components';
+
+const BudgetsContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding: 5%;
+  width: 80%;
+  border-bottom: 1px solid #e266b5;
+`;
+const BudgetsInfo = styled.div`
+  margin: 0 auto;
+`;
 
 function Budgets() {
   return (
-    <div className="budgets-container">
-      <Plot
-        data={[
-          {
-            mode: 'markers',
-            x: [1, 2, 3, 4],
-            y: [10, 11, 12, 13],
-            text: [
-              'Savings Progress<br>size: 40',
-              'Savings Improved<br>size: 60',
-              'Savings Really Improved<br>size: 80',
-              'Met Savings Goal<br>size: 100',
-            ],
-            marker: {
-              color: [
-                'rgb(93, 164, 214)',
-                'rgb(255, 144, 14)',
-                'rgb(44, 160, 101)',
-                'rgb(255, 65, 54)',
+    <BudgetsContainer>
+      <BudgetsInfo>
+        <Plot
+          data={[
+            {
+              mode: 'markers',
+              x: [1, 2, 3, 4],
+              y: [10, 11, 12, 13],
+              text: [
+                'Savings Progress<br>size: 40',
+                'Savings Improved<br>size: 60',
+                'Savings Really Improved<br>size: 80',
+                'Met Savings Goal<br>size: 100',
               ],
-              size: [40, 60, 80, 100],
+              marker: {
+                color: [
+                  'rgb(93, 164, 214)',
+                  'rgb(255, 144, 14)',
+                  'rgb(44, 160, 101)',
+                  'rgb(255, 65, 54)',
+                ],
+                size: [40, 60, 80, 100],
+              },
             },
-          },
-        ]}
-        layout={{
-          title: 'Bubble Chart Hover Text',
-          showlegend: false,
-          height: 600,
-          width: 600,
-        }}
-      />
-      <div className="Budgets-info">
+          ]}
+          layout={{
+            title: 'Bubble Chart Hover Text',
+            showlegend: false,
+            height: 600,
+            width: 600,
+          }}
+        />
+      </BudgetsInfo>
+
+      <BudgetsInfo>
         <h1>Your Savings this Month</h1>
         <p>You saved this much in this category this month.</p>
-      </div>
-    </div>
+      </BudgetsInfo>
+    </BudgetsContainer>
   );
 }
 
