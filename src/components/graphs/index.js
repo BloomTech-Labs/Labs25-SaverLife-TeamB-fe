deeb91ef28971aac26396f25775f8aab0a8918ef
Julia Vu <juliadvu@gmail.com>
2020-08-25 16:32:26 -0500

    Chore: updated import to SpendingPost graph

--- src/components/graphs/index.js
+++ src/components/graphs/index.js
@@ -1,6 +1,6 @@
 export { default as SavingsGraph } from './SavingsGraph';
 
-export { default as Budgets } from './Budgets';
+export { default as SpendingPost } from './SpendingPost';
 export { default as ComparedSavings } from './ComparedSavings';
 export { default as ComparedSpendings } from './ComparedSpendings';
 export { default as GraphCarousel } from './GraphCarousel';
