614365a8bf59f07b6787fa0c8bda138751389fa1
kwmorlock <kmorlockcodes@gmail.com>
2020-08-25 19:48:36 -0400

    chore:getByText-test-backtoaccount

--- src/__tests__/NotFoundPageButtons.test.js
+++ src/__tests__/NotFoundPageButtons.test.js
@@ -2,3 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { NotFoundPage } from '../components/pages/NotFound';
+
+describe('Loading Common Component', () => {
+  test('it should mount a button based on a link', () => {
+    const { getByText } = render(
+      <Router>
+        <NotFoundPage />
+      </Router>
+    );
+    const link = getByText(/Back To Account/i);
+    expect(link.textContent).toBe('Back To Account');
+  });
+});
