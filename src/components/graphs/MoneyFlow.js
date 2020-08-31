c0a55f83c16dd8b7f342213f4c5ef6299f1287ed
j721 <juliadvu@gmail.com>
2020-08-31 16:55:49 -0500

    Feat: updates to display Moneyflow graph from data science

---
+++ src/components/graphs/MoneyFlow.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
+import styled from 'styled-components';
+import Button from '../common/Button';
+import { getMoneyFlow } from '../../api';
+
+const MoneyFlowContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding: 5%;
+  width: 80%;
+  border-bottom: 5px solid #e266b5;
+`;
+const MoneyFlowInfo = styled.div`
+  margin: 0 auto;
+`;
+
+const initialState = {
+  data: [],
+  layout: {},
+};
+
+function MoneyFlow(props) {
+  const [data, setData] = useState(initialState);
+
+  console.log(data);
+
+  useEffect(() => {
+    function fetchDSData() {
+      getMoneyFlow(props.url, props.authState)
+        .then(res => {
+          setData(res);
+        })
+        .catch(err => {
+          setData({ data: null, err });
+          console.log(err);
+        });
+    }
+    fetchDSData();
+  }, [props.url, props.authState]);
+
+  return (
+    <MoneyFlowContainer>
+      <MoneyFlowInfo>
+        <h1>Your daily net income for this month.</h1>
+        <p>Here are your transactions and net income for this month.</p>
+      </MoneyFlowInfo>
+      <MoneyFlowInfo>
+        <Plot data={data.data} layout={data.layout} />
+      </MoneyFlowInfo>
+    </MoneyFlowContainer>
+  );
+}
+export default MoneyFlow;
