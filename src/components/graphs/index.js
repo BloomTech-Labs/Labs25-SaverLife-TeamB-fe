ade61c7865171e4ca30191799197eb48eb3971a1
j721 <juliadvu@gmail.com>
2020-09-02 12:00:47 -0500

    Chore: updates to import with Budgets graph

--- src/components/graphs/index.js
+++ src/components/graphs/index.js
@@ -1,6 +1,6 @@
 export { default as MoneyFlow } from './MoneyFlow';
 
 export { default as SpendingPost } from './SpendingPost';
-export { default as ComparedSavings } from './ComparedSavings';
+export { default as Budgets } from './Budgets';
 export { default as ComparedSpendings } from './ComparedSpendings';
 export { default as GraphCarousel } from './GraphCarousel';
