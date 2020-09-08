e34968e9e16ba7e4f181cdc01ea52d104f06196c
Steven Spencer <steve35spencer@gmail.com>
2020-09-08 12:03:04 -0400

    chore: created root file for Home styledcomponents

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'antd';
 import Nav from '../Nav/Nav';
-import styled from 'styled-components';
 import RenderGraphs from '../../graphs/RenderGraphs';
-
-import { useOktaAuth } from '@okta/okta-react/dist/OktaContext';
-
-const HeaderContainer = styled.div`
-  align-content: center;
-  margin: 0 auto;
-  width: 80%;
-`;
-const HomeWrapper = styled.div`
-  @media (max-width: 765px) {
-    text-align: center;
-    width: 100%;
-  }
-`;
+import { HomeWrapper, HeaderContainer } from './styles/HomeStyles';
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
 
-  // this function gets the width of the page, and renders either the graph carousel (mobile) or all of the graphs individually (desktop)
   return (
     <HomeWrapper>
       <Nav authService={authService} />
---
+++ src/components/pages/Home/styles/HomeStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  align-content: center;
+  margin: 0 auto;
+  width: 80%;
+`;
+export const HomeWrapper = styled.div`
+  @media (max-width: 765px) {
+    text-align: center;
+    width: 100%;
+  }
+`;
