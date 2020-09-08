b5653de6a609cbe43e7bed43a98b5bdbde0f5edf
Steven Spencer <steve35spencer@gmail.com>
2020-09-08 12:02:34 -0400

    chore: created root file for nav styledcomponents

--- src/components/pages/Nav/Nav.js
+++ src/components/pages/Nav/Nav.js
@@ -1,48 +1,10 @@
 import React from 'react';
-import { Button } from 'antd';
-import styled from 'styled-components';
-
-const NavWrapper = styled.div`
-  border-bottom: 8px solid rgb(189, 35, 125);
-  margin-bottom: 30px;
-`;
-
-const NavContents = styled.div`
-  margin: 0 auto;
-  padding: 30px;
-  display: flex;
-  width: 65%;
-  align-items: center;
-  justify-content: space-between;
-  @media (max-width: 765px) {
-    width: 100%;
-    flex-direction: column;
-  }
-`;
-
-const SLLogo = styled.img`
-  width: 170px;
-  @media (max-width: 765px) {
-    width: 100%;
-    margin-bottom: 10%;
-  }
-`;
-
-const LogoutButton = styled.button`
-  border: 1px solid rgb(189, 35, 125);
-  color: rgb(189, 35, 125);
-  border-radius: 12px;
-  font-size: 20px;
-  font-weight: 500;
-  background-color: white;
-  padding: 0px 30px;
-  height: 40px;
-  :hover {
-    color: white;
-    background-color: rgb(189, 35, 125);
-    border: 1px solid rgb(189, 35, 125);
-  }
-`;
+import {
+  NavWrapper,
+  NavContents,
+  SLLogo,
+  LogoutButton,
+} from './styles/NavStyles';
 
 function Nav({ authService }) {
   return (
