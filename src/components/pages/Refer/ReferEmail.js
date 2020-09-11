045cd999d92bb63b5edb3c3a7a0d5dfc41ef77e2
kwmorlock <kmorlockcodes@gmail.com>
2020-09-10 23:13:51 -0400

    feat/ReferEmail-headerAdded

--- src/components/pages/Refer/ReferEmail.js
+++ src/components/pages/Refer/ReferEmail.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
+import { HomeWrapper, HeaderContainer } from '../Home/styles/HomeStyles';
 
-const ReferEmail = () => {
+const ReferEmail = props => {
+  const { userInfo, authService } = props;
   return (
-    <div>
-      <h1>Refer a Friend!</h1>
-    </div>
+    <HomeWrapper>
+      <Nav authService={authService} />
+      <HeaderContainer>
+        <h1>Refer a Friend!</h1>
+      </HeaderContainer>
+    </HomeWrapper>
   );
 };
 
