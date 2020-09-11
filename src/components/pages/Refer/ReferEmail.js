069bd23eeb8680ecc7a502e010e6ec6d5cfce3b2
kwmorlock <kmorlockcodes@gmail.com>
2020-09-10 23:23:25 -0400

    feat/ReferEmail-emailFormCreated

--- src/components/pages/Refer/ReferEmail.js
+++ src/components/pages/Refer/ReferEmail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
 import { HomeWrapper, HeaderContainer } from '../Home/styles/HomeStyles';
+import { FormInput, FormButton } from '../../common';
 
 const ReferEmail = props => {
   const { userInfo, authService } = props;
@@ -9,6 +10,18 @@ const ReferEmail = props => {
       <Nav authService={authService} />
       <HeaderContainer>
         <h1>Refer a Friend!</h1>
+        <form onSubmit={e => e.preventDefault()}>
+          <FormInput
+            placeholder="Email"
+            name="email"
+            labelId="Friend's Email"
+          />
+          <FormButton
+            buttonText="Submit"
+            classType="default"
+            disabled="false"
+          />
+        </form>
       </HeaderContainer>
     </HomeWrapper>
   );
