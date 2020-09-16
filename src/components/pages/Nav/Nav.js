129c3331f9854d1e95c0f6c0869767af6e85f382
Henry Nelson <Henry.Nelson89@gmail.com>
2020-09-16 13:01:55 -0700

    chore/ changed anchor to link

--- src/components/pages/Nav/Nav.js
+++ src/components/pages/Nav/Nav.js
@@ -7,9 +7,10 @@ function Nav({ authService }) {
   return (
     <NavWrapper>
       <NavContents>
-        <a href="#">
+        <Link to={'/'}>
           <SLLogo src="https://www.saverlife.org/assets/logo-saverlife-a4b213a1d9e8e51559d7f70d9f479f1473f536e12c8c4543654d5b3964004b0f.svg" />
-        </a>
+        </Link>
+
         <Link to={'/budget'}>
           <Button className="budget" type="primary">
             Budget
