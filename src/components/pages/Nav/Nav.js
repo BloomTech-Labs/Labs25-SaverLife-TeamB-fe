cd547bfd2a98d50a491bd1af6920b3038eaa651e
Henry Nelson <Henry.Nelson89@gmail.com>
2020-09-16 10:32:32 -0700

    /feat added a budgets button

--- src/components/pages/Nav/Nav.js
+++ src/components/pages/Nav/Nav.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import {
-  NavWrapper,
-  NavContents,
-  SLLogo,
-  LogoutButton,
-} from './styles/NavStyles';
+import { Link } from 'react-router-dom';
+import { Budgets } from '../Budgets';
+import { NavWrapper, NavContents, SLLogo, Button } from './styles/NavStyles';
 
 function Nav({ authService }) {
   return (
@@ -13,13 +10,19 @@ function Nav({ authService }) {
         <a href="#">
           <SLLogo src="https://www.saverlife.org/assets/logo-saverlife-a4b213a1d9e8e51559d7f70d9f479f1473f536e12c8c4543654d5b3964004b0f.svg" />
         </a>
-        <LogoutButton
+        <Link to={'/budget'}>
+          <Button className="budget" type="primary">
+            Budget
+          </Button>
+        </Link>
+
+        <Button
           className="logout"
           type="primary"
           onClick={() => authService.logout()}
         >
           Logout
-        </LogoutButton>
+        </Button>
       </NavContents>
     </NavWrapper>
   );
