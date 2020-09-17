3ef7a5b93d2e25cf14c5c76c6634c83ddb7b9042
j721 <juliadvu@gmail.com>
2020-09-17 16:22:29 -0500

    Chore: added in media query for Headercontainer div component

--- src/components/pages/Home/styles/HomeStyles.js
+++ src/components/pages/Home/styles/HomeStyles.js
@@ -5,6 +5,11 @@ export const HeaderContainer = styled.div`
   text-align: center;
   margin: 0 auto;
   width: 80%;
+  @media (max-width: 765px) {
+    text-align: center;
+    width: 100%;
+    padding: 5%;
+  }
 `;
 export const HomeWrapper = styled.div`
   @media (max-width: 765px) {
