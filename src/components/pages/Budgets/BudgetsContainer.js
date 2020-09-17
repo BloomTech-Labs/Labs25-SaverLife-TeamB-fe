674c2e37753227a68facf6fb176e2fa1b3ed2675
Henry Nelson <Henry.Nelson89@gmail.com>
2020-09-17 14:57:01 -0700

    fix/ created budgets contaner for state

---
+++ src/components/pages/Budgets/BudgetsContainer.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+
+import Budgets from './Budgets';
+
+function BudgetsContainer({ LoadingComponent }) {
+  console.log('ERRROR');
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
+  // eslint-disable-next-line
+  const [memoAuthService] = useMemo(() => [authService], []);
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    memoAuthService
+      .getUser()
+      .then(info => {
+        // if user is authenticated we can use the authService to snag some user info.
+        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
+        if (isSubscribed) {
+          setUserInfo(info);
+        }
+      })
+      .catch(err => {
+        isSubscribed = false;
+        return setUserInfo(null);
+      });
+    return () => (isSubscribed = false);
+  }, [memoAuthService]);
+  return (
+    <>
+      {authState.isAuthenticated && !userInfo && (
+        <LoadingComponent message="Fetching user profile..." />
+      )}
+      {authState.isAuthenticated && userInfo && (
+        <Budgets
+          userInfo={userInfo}
+          authService={authService}
+          url={process.env.REACT_APP_API_URI + 'data/futureBudget'}
+          authState={authState}
+        />
+      )}
+    </>
+  );
+}
+
+export default BudgetsContainer;
