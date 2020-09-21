efa7decb4628c31c2cf57d36cc1b5578c92016d5
kwmorlock <kmorlockcodes@gmail.com>
2020-09-21 13:15:51 -0400

    feat/refer-handleSubmit

--- src/components/pages/Refer/ReferEmail.js
+++ src/components/pages/Refer/ReferEmail.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from '../Nav/Nav';
 import { HomeWrapper, HeaderContainer } from '../Home/styles/HomeStyles';
 import { FormInput, FormButton } from '../../common';
+import axios from 'axios';
 
 const initialState = {
   to: '',
@@ -14,6 +15,18 @@ const ReferEmail = props => {
       ...addedEmail,
       [e.target.name]: e.target.value,
     });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_API_URI}refer`, addedEmail)
+      .then(res => {
+        console.log(res, 'added email data working');
+        setAddedEmail(res.body.to);
+      })
+      .catch(err => console.log(err, 'Email data failed to return'));
+    props.history.push('/refer');
+    console.log(addedEmail);
   };
   const { userInfo, authService } = props;
   return (
