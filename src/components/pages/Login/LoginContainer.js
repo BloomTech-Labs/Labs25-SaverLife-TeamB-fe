5af474bfaa149a06a9472fd86a448f2ccd659b62
j721 <juliadvu@gmail.com>
2020-09-21 22:44:38 -0500

    Chore: added in Logo image

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 import { LoginHolder } from './styles/LoginStyles';
-
+import Logo from '../../common/Logo.png';
 import { config } from '../../../utils/oktaConfig';
 
 const LoginContainer = () => {
@@ -18,11 +18,11 @@ const LoginContainer = () => {
       },
       features: { registration: false },
       // turning this feature on allows your widget to use Okta for user registration
-      logo: 'path-to-your-logo',
+      logo: Logo,
       // add your custom logo to your signing/register widget here.
       i18n: {
         en: {
-          'primaryauth.title': 'Welcome to SaverLife',
+          'primaryauth.title': 'Welcome to SaverLife!',
           // change title for your app
         },
       },
