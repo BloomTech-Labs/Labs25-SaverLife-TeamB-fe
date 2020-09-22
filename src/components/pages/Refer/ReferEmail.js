700f4f4d444324a6a657c4bd34ad54efbee13d8e
kwmorlock <kmorlockcodes@gmail.com>
2020-09-22 13:09:25 -0400

    chore:refer-stylingPicResize

--- src/components/pages/Refer/ReferEmail.js
+++ src/components/pages/Refer/ReferEmail.js
@@ -84,8 +84,8 @@ const ReferEmail = props => {
         <img
           src="https://images.unsplash.com/photo-1586034679970-cb7b5fc4928a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
           className="piggy"
-          width="1000"
-          height="600"
+          width="700"
+          height="500"
         />
       </HeaderContainer>
     </HomeWrapper>
